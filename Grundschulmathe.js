6f546aac0afd27b40c27a090f9355299ce7e9144
unknown <julia.wienkop@gmail.com>
2021-02-05 09:12:59 +0100

    fullfills task c

--- Grundschulmathe.js
+++ Grundschulmathe.js
@@ -1,31 +1,34 @@
 alert("Grundschulmathe");
 
-var min = 1;
-var max = 20;
+var testsCount = 0;
+var levelCount = 0;
 
-function rand(min, max)
-{
+//Ermitteln der Zufallszahl
+function rand(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function rechenaufgabe() {
+    let correctCount = 0;
+    for (let i = 1; i <= 5; i++) 
+    {
+        switch (levelCount) {
+            case 0:
+                rechenzeichen = "+";
 
+                var number1 = rand(1, 20);
+                var number2 = rand(1, 20);
+                var result = number1 + number2;
+                break;
+        }
+        var feed = prompt("Wie viel ist " + number1 + rechenzeichen + number2);
 
-function zufall() {
-    var number1 = rand(1, 20);
-    var number2 = rand(1, 20);
-    var result = number1 + number2;
-
-    var Ergebnis = prompt("Wie viel ist " + number1 + " + " + number2);
-
-    if (Ergebnis != null) {
-        document.getElementById("demo").innerHTML =
-            "Your result was " + Ergebnis;
-    }
-
-    if (result == Ergebnis) {
-        alert("true");
-    }
-    else {
-        alert("false");
+        if (result == feed) {
+            correctCount++;
+            alert("Richtig! (" + correctCount + "/5) ");
+        }
+        else {
+            alert("Nicht richtig! (" + correctCount + "/5)");
+        }
     }
 }
