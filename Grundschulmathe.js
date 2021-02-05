ca81ec07037c54da96996634a5f8eb27857697b4
unknown <julia.wienkop@gmail.com>
2021-02-05 09:21:56 +0100

    fullfills task d

--- Grundschulmathe.js
+++ Grundschulmathe.js
@@ -20,6 +20,15 @@ function rechenaufgabe() {
                 var number2 = rand(1, 20);
                 var result = number1 + number2;
                 break;
+            
+            case 1:
+                    rechenzeichen = "-"
+    
+                    var number1 = rand(1, 20);
+                    var number2 = rand(1, number1);
+    
+                    var result = number1 - number2;
+                    break;
         }
         var feed = prompt("Wie viel ist " + number1 + rechenzeichen + number2);
 
@@ -31,4 +40,19 @@ function rechenaufgabe() {
             alert("Nicht richtig! (" + correctCount + "/5)");
         }
     }
+    if (correctCount >= 4) {
+        levelCount++;
+        alert("Well, done! You have passed level #" + levelCount);
+    }
+    else {
+        alert("To reach the next level you need to try it again");
+    }
+
+    if (levelCount == 2) {
+        levelCount = 0;
+        document.getElementById("demo").innerHTML = "You've finished all levels";
+    }
+    else
+        document.getElementById("demo").innerHTML =
+            "You had " + correctCount + "/5 correct answers on this level";
 }
